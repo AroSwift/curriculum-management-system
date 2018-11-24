baceaa23b8629dc2959c9c169ff3532010776257
Aaron Barlow <abarlow505@gmail.com>
2018-11-24 10:57:06 -0500

    Remember current uri link

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,23 +1,36 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"
-          integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <title>
         @yield('title')
     </title>
 </head>
 <body>
 
-    <nav class="nav nav-inline">
-        <a class="nav-link active" href="/users">Users</a>
-        <a class="nav-link" href="/majors">Majors</a>
-        <a class="nav-link" href="/courses">Courses</a>
-        <a class="nav-link" href="/roles">Roles</a>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Cirriculum Management System</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item {{ Request::is('courses') || Request::is('courses/*') ? 'active' : '' }}">
+                    <a class="nav-link" href="/courses">Courses</a>
+                </li>
+                <li class="nav-item {{ Request::is('majors') || Request::is('majors/*') ? 'active' : '' }}">
+                    <a class="nav-link" href="/majors">Majors</a>
+                </li>
+                <li class="nav-item {{ Request::is('roles') || Request::is('roles/*') ? 'active' : '' }}">
+                    <a class="nav-link" href="/roles">Roles</a>
+                </li>
+                <li class="nav-item {{ Request::is('users') || Request::is('users/*') ? 'active' : '' }}">
+                    <a class="nav-link" href="/users">Users</a>
+                </li>
+            </ul>
+        </div>
     </nav>
 
     <div class="container-fluid">
